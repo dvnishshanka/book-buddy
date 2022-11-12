e0ac7460178a0e7a901fed50ddf3f666c100c00b
sara pulido <sara01rizo@gmail.com>
2022-11-12 16:18:47 +0100

    Add squeleton for dashboard page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,13 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home ]
+  skip_before_action :authenticate_user!, only: [:home]
 
   def home
+    @books = Book.all.sample(2)
+    @renting = Renting.all
+  end
+
+  def dashboard
+    current_user_books
+    current_user_orders
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,5 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get "dashboard/:id", to: "pages#dashboard", as: :dashboard
 end
