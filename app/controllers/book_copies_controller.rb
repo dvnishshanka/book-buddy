e39ce35ff524fb5d482103e1be8112ee90a446d5
Andrew Grant <andrewgrant2009@hotmail.com>
2022-11-15 21:02:41 +0100

    copies can be added need to work on styling now

--- app/controllers/book_copies_controller.rb
+++ app/controllers/book_copies_controller.rb
@@ -1,4 +1,8 @@
 class BookCopiesController < ApplicationController
+  def index
+    @book_copies = BookCopy.all
+  end
+
   def new
     @book_copy = BookCopy.new
   end
@@ -7,6 +11,10 @@ class BookCopiesController < ApplicationController
     @book_copy = BookCopy.new(book_copy_params)
     @book_copy.user_id = current_user
     @book_copy.save
+  end
+
+  def show
+    @book_copy = BookCopy.find(params[:id])
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-
+  resources :books
   resources :book_copies, only: [:new, :create, :index, :show]
 
   get "dashboard/:id", to: "pages#dashboard", as: :dashboard
