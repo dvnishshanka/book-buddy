a843533014c96ff207363eea70c84293003b1aad
Vidushani Nishshanka <dvnishshanka@gmail.com>
2022-11-16 14:19:16 +0100

    Create Book Copy

--- app/controllers/book_copies_controller.rb
+++ app/controllers/book_copies_controller.rb
@@ -1,29 +1,33 @@
 class BookCopiesController < ApplicationController
+  before_action :set_book_copy, only: %i[show]
+
   def index
     @book_copies = BookCopy.all
   end
 
   def new
     @book_copy = BookCopy.new
+    @books = Book.all
   end
 
   def create
     @book_copy = BookCopy.new(book_copy_params)
-    @book_copy.user_id = current_user
-      if @book_copy.save
-      redirect_to root_path
+    @book_copy.user = current_user
+    if @book_copy.save
+      redirect_to book_path(@book_copy.book)
     else
+      render :new, status: :unprocessable_entity
     end
-  
   end
 
   def show
-    @book_copy = BookCopy.find(params[:id])
-    @book_copy.user_id = current_user.id
-    @book_copy.save
   end
 
   private
+
+  def set_book_copy
+    @book_copy = BookCopy.find(params[:id])
+  end
 
   def book_copy_params
     params.require(:book_copy).permit(:comments, :availability_status, :book_id)
