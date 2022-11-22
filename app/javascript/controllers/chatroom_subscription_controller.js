0d0798f1286a834167ecd974a3be3a79bb777215
sara pulido <sara01rizo@gmail.com>
2022-11-22 21:08:51 +0100

    refactoring general channel

--- app/javascript/controllers/chatroom_subscription_controller.js
+++ app/javascript/controllers/chatroom_subscription_controller.js
@@ -1,5 +1,5 @@
 import { Controller } from "@hotwired/stimulus"
-// import { createConsumer } from "@rails/actioncable"
+import { createConsumer } from "@rails/actioncable"
 
 // Connects to data-controller="chatroom-subscription"
 export default class extends Controller {
@@ -8,8 +8,13 @@ export default class extends Controller {
   connect() {
     this.channel = createConsumer().subscriptions.create(
       { channel: "ChatroomChannel", id: this.chatroomIdValue },
-      { received: data => console.log(data) }
+      { received: (data) => {this.#insertMessage(data)} }
+
     )
-    console.log(`Subscribed to the chatroom with the id ${this.chatroomIdValue}.`)
+  }
+
+  #insertMessage(data) {
+    this.messagesTarget.insertAdjacentHTML("beforeend", data)
+    this.messagesTarget.scrollTo(0, this.messagesTarget.scrollHeight)
   }
 }
