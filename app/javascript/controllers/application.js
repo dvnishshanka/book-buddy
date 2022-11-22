0475e720aefb4db17312355c66e0fdf028f036e8
Andrew Grant <andrewgrant2009@hotmail.com>
2022-11-22 10:32:43 +0100

    new colors and flip card feature

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,8 +1,5 @@
 import { Application } from "@hotwired/stimulus"
-import ReadMore from 'stimulus-read-more'
 
-const application = Application.start()
-application.register('read-more', ReadMore)
 
 // Configure Stimulus development experience
 application.debug = false
