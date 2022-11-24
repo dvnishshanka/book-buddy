8e6040e8a098218ce8b98a2664cc3e09c279caa7
Andrew Grant <andrewgrant2009@hotmail.com>
2022-11-24 18:37:04 +0100

    main-book-card-style

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,6 @@
 class Order < ApplicationRecord
   belongs_to :user
-  belongs_to :book_copy
+  belongs_to :book_copy, dependent: :destroy
   validates :start_date, :end_date, presence: true
   validate :end_date_after_start_date
 
