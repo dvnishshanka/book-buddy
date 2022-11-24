94611284c09e35c3ea67d96e769875a43127594d
sara pulido <sara01rizo@gmail.com>
2022-11-24 17:11:51 +0100

    Task: chatroom styling

--- db/schema.rb
+++ db/schema.rb
@@ -76,6 +76,12 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_19_145403) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "chats", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "messages", force: :cascade do |t|
     t.string "content"
     t.bigint "chatroom_id", null: false
