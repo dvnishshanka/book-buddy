d6b9b8636e4dc6353cab83c583eebac71979fbfb
sara pulido <sara01rizo@gmail.com>
2022-11-25 17:59:59 +0100

    Add scroll controller for messages

--- app/javascript/controllers/chatroom_subscription_controller.js
+++ app/javascript/controllers/chatroom_subscription_controller.js
@@ -3,49 +3,28 @@ import { createConsumer } from "@rails/actioncable"
 
 // Connects to data-controller="chatroom-subscription"
 export default class extends Controller {
-  static values = { chatroomId: Number, currentUserId: Number }
+  static values = { chatroomId: Number }
   static targets = ["messages"]
+
   connect() {
     this.channel = createConsumer().subscriptions.create(
       { channel: "ChatroomChannel", id: this.chatroomIdValue },
-      { received: (data) => {this.#insertMessage(data)} }
-
+      { received: data => this.#insertMessageAndScrollDown(data) }
     )
+    console.log(`Subscribed to the chatroom with the id ${this.chatroomIdValue}.`)
   }
 
-  #insertMessage(data) {
-    const currentUserIsSender = this.currentUserIdValue === data.sender_id
+  #insertMessageAndScrollDown(data) {
     this.messagesTarget.insertAdjacentHTML("beforeend", data)
     this.messagesTarget.scrollTo(0, this.messagesTarget.scrollHeight)
   }
 
-  #buildMessageElement(currentUserIsSender, message) {
-    return `
-      <div class="message-row d-flex ${this.#justifyClass(currentUserIsSender)}">
-        <div class="${this.#userStyleClass(currentUserIsSender)}">
-          ${message}
-        </div>
-      </div>
-    `
+  resetForm(event) {
+    event.target.reset()
   }
 
-  #justifyClass(currentUserIsSender) {
-    return currentUserIsSender ? "justify-content-end" : "justify-content-start"
-  }
-
-  #userStyleClass(currentUserIsSender) {
-    return currentUserIsSender ? "sender-style" : "receiver-style"
-  }
-
-  #insertMessageAndScrollDown(data) {
-    // Logic to know if the sender is the current_user
-    const currentUserIsSender = this.currentUserIdValue === data.sender_id
-
-    // Creating the whole message from the `data.message` String
-    const messageElement = this.#buildMessageElement(currentUserIsSender, data.message)
-
-    // Inserting the `message` in the DOM
-    this.messagesTarget.insertAdjacentHTML("beforeend", messageElement)
-    this.messagesTarget.scrollTo(0, this.messagesTarget.scrollHeight)
+  disconnect() {
+    console.log("Unsubscribed from the chatroom")
+    this.channel.unsubscribe()
   }
 }
