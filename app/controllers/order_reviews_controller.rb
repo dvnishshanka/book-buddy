0e8e7651cf9186a09a50ca3be12dd308c3e67719
Vidushani Nishshanka <dvnishshanka@gmail.com>
2022-11-28 21:27:55 +0100

    User review

--- app/controllers/order_reviews_controller.rb
+++ app/controllers/order_reviews_controller.rb
@@ -1,2 +1,24 @@
 class OrderReviewsController < ApplicationController
+  def new
+    @order_review = OrderReview.new
+    @order_review.order = set_order
+    @order_reviews = OrderReview.where(user_id: set_order.book_copy.user_id)
+  end
+
+  def create
+    @order_review = OrderReview.new(order_review_params)
+    @order_review.order = set_order
+    @order_review.user_id = set_order.book_copy.user_id
+    @order_review.save
+  end
+
+  private
+
+  def set_order
+    @order = Order.find(params[:order_id])
+  end
+
+  def order_review_params
+    params.require(:order_review).permit(:rating, :content)
+  end
 end
--- app/models/order_review.rb
+++ app/models/order_review.rb
@@ -1,3 +1,4 @@
 class OrderReview < ApplicationRecord
   belongs_to :order
+  belongs_to :user
 end
