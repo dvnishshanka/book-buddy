d9ff4bc411698083763c7c27dfaabf0b4cbf438e
Vidushani Nishshanka <dvnishshanka@gmail.com>
2022-11-29 09:13:26 +0100

    Ajaxify order review

--- app/controllers/order_reviews_controller.rb
+++ app/controllers/order_reviews_controller.rb
@@ -2,14 +2,22 @@ class OrderReviewsController < ApplicationController
   def new
     @order_review = OrderReview.new
     @order_review.order = set_order
-    @order_reviews = OrderReview.where(user_id: set_order.book_copy.user_id)
+    @order_reviews = OrderReview.where(user_id: set_order.book_copy.user_id).order(created_at: :desc)
   end
 
   def create
     @order_review = OrderReview.new(order_review_params)
     @order_review.order = set_order
     @order_review.user_id = set_order.book_copy.user_id
-    @order_review.save
+
+    respond_to do |format|
+      if @order_review.save
+        format.html { redirect_to new_book_copy_order_order_review_path(@order_review.order) }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+      end
+      format.json
+    end
   end
 
   private
--- app/controllers/user_profiles_controller.rb
+++ app/controllers/user_profiles_controller.rb
@@ -1,7 +1,7 @@
 class UserProfilesController < ApplicationController
   def show
     set_user
-    @order_review = OrderReview.new
+    @order_reviews = OrderReview.where(user_id: @user.id).order(created_at: :desc)
   end
 
   private
