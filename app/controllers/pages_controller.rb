00e32ee96904b13cb277c54ef7c7746ac34d161a
Andrew Grant <andrewgrant2009@hotmail.com>
2022-12-03 17:10:33 +0100

    adding new divs for display

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -12,13 +12,14 @@ class PagesController < ApplicationController
         sql_query = "title ILIKE :query OR author ILIKE :query OR description ILIKE :query"
         @books = Book.where(sql_query, query: "%#{params[:query]}%")
       else
-        @books = Book.all
+        @books = Book.all.sample(14)
       end
       respond_to do |format|
         format.html
         format.text { render partial: "books/list", locals: { books: @books }, formats: [:html] }
       end
-      @horrorbooks = Book.where(category: "Horror")
+      @horrorbooks = Book.all.where(title: ["Stories of Your Life and Others", "The Handmaid's Tale", "The Green Mile", "The Man in the High Castle", "The Great Gatsby", "Call Me by Your Name"])
+      @irishbooks = Book.all.where(title: ["All Down Darkness Wide", "Listen to the Land Speak", "A Portrait of the Artist as a Young Man", "The Island that Dared"])
 
     end
   end
