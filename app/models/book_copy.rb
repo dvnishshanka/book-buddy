c7188b6c471b86b5c60ec02a77258cc31ed8b962
Vidushani Nishshanka <dvnishshanka@gmail.com>
2022-12-08 21:11:05 +0100

    Remove dependency from book_copy

--- app/models/book_copy.rb
+++ app/models/book_copy.rb
@@ -1,6 +1,6 @@
 class BookCopy < ApplicationRecord
   belongs_to :book
-  belongs_to :user, dependent: :destroy
+  belongs_to :user
   has_many :orders, dependent: :destroy
   has_many :chatrooms, dependent: :destroy
   validates :condition, presence: true, inclusion: { in: [1, 2, 3, 4, 5], allow_nil: false }
